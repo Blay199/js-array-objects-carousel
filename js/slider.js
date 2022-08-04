c13c1c2cc65c368d8673943fe1a2b8760cefd965
Blay199 <a.sorrentino03@gmail.com>
2022-08-04 17:48:25 +0200

    Problema risolto

--- js/slider.js
+++ js/slider.js
@@ -14,6 +14,40 @@
 	// rimetto active
 	// se indiceActive uguale a slidesEl
 		// indexActive = 0
+		const slides = [
+			{
+				url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+				title: 'Svezia',
+				description:
+					'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
+			},
+		
+			{
+				url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+				title: 'Perù',
+				description:
+					'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
+			},
+		
+			{
+				url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+				title: 'Chile',
+				description:
+					'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
+			},
+			{
+				url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+				title: 'Argentina',
+				description:
+					'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
+			},
+			{
+				url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+				title: 'Colombia',
+				description:
+					'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
+			},
+		]
 
 
 
@@ -21,56 +55,22 @@
 // recupero elemento slides wrapper
 const slideWrapperEl = document.querySelector('.slides-wrapper')
 
-slides.forEach(element => {
+slides.forEach((element, indice) => {
 	let slideEl = `
-	<li class="slide active">
-            <img src="${url}" alt="">
+	<li class="slide`
+	if (indice === 0) {
+		slideEl += ` active`
+	}
+	slideEl +=`">
+            <img src="${element.url}" alt="">
             <div class="slide__content">
-              <h3 class="slide__title">${title}</h3>
-              <p class="slide__description">${description}</p>
+              <h3 class="slide__title">${element.title}</h3>
+              <p class="slide__description">${element.description}</p>
             </div>
           </li>
 	`
-	slideWrapperEl.push(slideEl)
+	slideWrapperEl.innerHTML += slideEl
 });
 
-let indexActive = 0;
-
-let slidesArray = [document.querySelectorAll('.slide')]
-slidesEl[indexActive].classList.add('active')
 
 // console.log('slider')
-const slides = [
-	{
-		url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
-		title: 'Svezia',
-		description:
-			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
-	},
-
-	{
-		url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
-		title: 'Perù',
-		description:
-			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
-	},
-
-	{
-		url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
-		title: 'Chile',
-		description:
-			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
-	},
-	{
-		url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
-		title: 'Argentina',
-		description:
-			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
-	},
-	{
-		url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
-		title: 'Colombia',
-		description:
-			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
-	},
-]
